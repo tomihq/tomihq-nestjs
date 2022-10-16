dea9d848d114766270b976fd07a88ee7cdd20492
Tomás Hernández <hernandeztomas584@gmail.com>
2022-10-15 21:00:32 -0300

    revert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+<<<<<<< HEAD
 import { BrandsModule } from './brands/brands.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [CarsModule, BrandsModule, SeedModule],
+=======
+
+@Module({
+  imports: [CarsModule],
+>>>>>>> parent of 80c96c2 (add: brands crud with entities)
   controllers: [],
   providers: [],
   exports: [],
